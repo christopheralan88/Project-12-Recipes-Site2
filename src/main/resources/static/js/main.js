a260a70ba720109272f22cea58428413f3b3e1bd
christopheralan88 <christopheralan88@gmail.com>
2017-07-09 00:32:35 -0400

    working on JS script to add ingredient rows

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,19 +1,25 @@
 $('#add-ingredient').on('click', function () {
+    console.log('Button was pressed');
     var newIndex = $( '.ingredient-row' ).length;
 
     var html = '<div class="ingredient-row" id="ingredientNumber' + newIndex + '">' +
-                '<input type="hidden" id="ingredients' + newIndex +'.id}" name="ingredients[' + newIndex + '].name"/>' +
                     '<div class="prefix-20 grid-30">' +
+                    '<input type="hidden" value="' + newIndex + '" id="ingredients' + newIndex +'.id}" name="ingredients[' + newIndex + '].id"/>' +
                         '<p>' +
                             '<input id="ingredients' + newIndex + '.name" name="ingredients[' + newIndex + '].name">' +
                         '</p>' +
                     '</div>' +
-                    '<div class="grid-10 suffix-10">' +
+                    '<div class="grid-30">' +
                         '<p>' +
-                            '<p><input style="width: 80%" id="ingredients' + newIndex + '.measure" name="ingredients['+ newIndex + '].measure"></p>' +
+                            '<input id="ingredients' + newIndex + '.condition}" name="ingredients[' + newIndex + '].condition}"/>' +
                         '</p>' +
                     '</div>' +
-                '</div>'
+                    '<div class="grid-10 suffix-10">' +
+                        '<p>' +
+                            '<p><input id="ingredients' + newIndex + '.measure" name="ingredients['+ newIndex + '].measure"></p>' +
+                        '</p>' +
+                    '</div>' +
+                '</div>';
 
     $( "#addIngredientButton" ).before(html);
 });
