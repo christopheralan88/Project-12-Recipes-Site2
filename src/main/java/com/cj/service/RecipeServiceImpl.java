17e7aa0ba693aab5d6308d4ee37fcc15b8b2ef1b
christopheralan88 <christopheralan88@gmail.com>
2017-07-20 23:01:32 -0400

    Able to delete favorited recipe

--- src/main/java/com/cj/service/RecipeServiceImpl.java
+++ src/main/java/com/cj/service/RecipeServiceImpl.java
@@ -2,6 +2,7 @@ package com.cj.service;
 
 import com.cj.dao.RecipeDao;
 import com.cj.model.Recipe;
+import com.cj.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +12,8 @@ import java.util.List;
 public class RecipeServiceImpl implements RecipeService {
     @Autowired
     private RecipeDao recipeDao;
+    @Autowired
+    private UserService userService;
 
     @Override
     public Iterable<Recipe> findAll() {
@@ -29,6 +32,16 @@ public class RecipeServiceImpl implements RecipeService {
 
     @Override
     public void delete(Recipe recipe) {
+        List<User> users = (List<User>) userService.findAll();
+        for (User user : users) {
+            for (Recipe r : user.getFavorites()) {
+                if (r.getId().equals(recipe.getId())) {
+                    user.getFavorites().remove(r);
+                    break;
+                }
+            }
+            userService.save(user);
+        }
         recipeDao.delete(recipe);
     }
 
